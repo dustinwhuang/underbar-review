6ac5afc92d351c8dfaafe93244cfd23de0ed6483
Hack Reactor Student <student@hackreactor.com>
2017-10-10 16:27:15 -0700

    Add implementations for extend and defaults

--- src/underbar.js
+++ src/underbar.js
@@ -244,11 +244,26 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    _.each(arguments, function(eachObj) {
+      _.each(eachObj, function(sourceVal, sourceKey) {
+        obj[sourceKey] = sourceVal;
+      });
+    });
+
+    return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
   _.defaults = function(obj) {
+    _.each(arguments, function(eachObj) {
+      _.each(eachObj, function(sourceVal, sourceKey) {
+        if (!obj.hasOwnProperty(sourceKey)) {
+          obj[sourceKey] = sourceVal;
+        }
+      });
+    });
+    return obj;
   };
 
 
